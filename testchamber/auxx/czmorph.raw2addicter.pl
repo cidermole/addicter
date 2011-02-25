40b7e8f6691aab56dc0db11731913f5fcc5c2317
fishel <fishel@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-02-25 19:32:49 +0000

    fixed lemmas in the data, repeated analysis -- not a big difference
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@1701 85e76b90-d0e1-47da-8fde-e7818187306d

--- testchamber/auxx/czmorph.raw2addicter.pl
+++ testchamber/auxx/czmorph.raw2addicter.pl
@@ -7,14 +7,18 @@ binmode(STDOUT, ":utf8");
 
 while (<STDIN>) {
 	s/\n//g;
-	
+	$_ = lc($_);
+
 	my @newwords = ();
 	
 	for my $word (split(/ /)) {
 		my ($form, $lemma, $tag) = split(/\|/, $word);
 		
-		if ($lemma =~ /^([[:alnum:]]+)[^[:alnum:]].*$/) {
+		if ($lemma =~ /^([[:alnum:]]+)([^[:alnum:]].*)$/) {
 			$lemma = $1;
+			my $fixCode = $2;
+			
+			$lemma = tryFixLemma($lemma, $fixCode);
 		}
 		
 		if ($tag =~ /^[ACDNVX]/) {
@@ -32,4 +36,20 @@ while (<STDIN>) {
 	
 	print join(" ", @newwords);
 	print "\n";
+}
+
+#####
+#
+#####
+sub tryFixLemma {
+	my ($lemma, $fixCode) = @_;
+	
+	#zaprášený_^(*3it)
+	if ($fixCode =~ /\(\*([0-9]+)([^)]*)\)/) {
+		my ($size, $repl) = ($1, $2);
+		
+		$lemma =~ s/.{$size}$/$repl/g;
+	}
+	
+	return $lemma;
 }
