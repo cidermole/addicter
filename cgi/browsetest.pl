30cbc643f02722c23f17c61ec709c1f96daecd47
zeman <zeman@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-05-09 08:57:14 +0000

    browsetest.pl rewritten. It now displays system hypothesis as well.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@1984 85e76b90-d0e1-47da-8fde-e7818187306d

--- cgi/browsetest.pl
+++ cgi/browsetest.pl
@@ -39,23 +39,15 @@ if(exists($config{experiment}))
         'HA'  => "${path}test.system.ali"
     );
     print("  <h1>Test Data of $config{experiment}</h1>\n");
-    my $numsnt = 1; ###!!!
-    my $plural = $numsnt>1 ? 's' : '';
+    # How many lines (sentences) are there in the test data?
+    my $numsnt = count_lines($files{S});
     if($numsnt>0)
     {
         my $example;
         # So what do we need to read?
-        my $sntno = 1; ###!!!
-        my ($srcfile, $tgtfile, $alifile);
-        ###!!! Tohle jsem okopíroval z example.pl, ale umí to jen dvě verze věty najednou.
-        ###!!! Tady bych potřeboval současně zdroj, referenci a hypotézu.
-        $srcfile = 'S';
-        $tgtfile = 'R';
-        $alifile = 'RA';
-        ###!!!$tgtfile = 'H';
-        ###!!!$alifile = 'HA';
-        print("  <p>This is the test sentence number $sntno:</p>\n");
-        print(sentence_to_table(\%files, $sntno, $srcfile, $tgtfile, $alifile));
+        my $sntno = $config{sntno}>0 ? $config{sntno} : 1;
+        print(get_navigation($sntno, $numsnt));
+        print(sentence_to_table($sntno, $files{S}, $files{R}, $files{RA}, $files{H}, $files{HA}));
     }
 }
 else
@@ -75,115 +67,162 @@ print("</html>\n");
 
 
 #------------------------------------------------------------------------------
+# Generates navigation information: sentence number, prev/next links.
+#------------------------------------------------------------------------------
+sub get_navigation
+{
+    my $sntno = shift; # number of current sentence (first sentence has number 1)
+    my $numsnt = shift; # number of sentences in file
+    my $html;
+    $html .= "  <p>";
+    $html .= "This is the test sentence number $sntno of $numsnt.";
+    # Provide links to the preceding and the following sentence.
+    my @links;
+    my $previous = $sntno-1;
+    my $next = $sntno+1;
+    push(@links, $previous>0 ? "<a href='browsetest.pl?experiment=$config{experiment}&amp;sntno=$previous'>previous</a>" : 'previous');
+    push(@links, $next<$numsnt ? "<a href='browsetest.pl?experiment=$config{experiment}&amp;sntno=$next'>next</a>" : 'next');
+    $html .= " Go to [".join(' | ', @links)."].";
+    $html .= "</p>\n";
+    return $html;
+}
+
+
+
+#------------------------------------------------------------------------------
 # Generates a HTML table with a sentence pair/triple (for test data, triples of
 # source, reference and hypothesis may be available).
 #------------------------------------------------------------------------------
 sub sentence_to_table
 {
     # This function accesses the global hashes %config and %prevod.
-    my $files = shift;
     my $sntno = shift;
     my $srcfile = shift;
     my $tgtfile = shift;
     my $alifile = shift;
+    my $hypfile = shift;
+    my $halifile = shift;
     my $html;
-    my $srcline = get_nth_line($files->{$srcfile}, $sntno);
-    my $tgtline = get_nth_line($files->{$tgtfile}, $sntno);
-    my $aliline = get_nth_line($files->{$alifile}, $sntno);
+    my $srcline = get_nth_line($srcfile, $sntno);
+    my $tgtline = get_nth_line($tgtfile, $sntno);
+    my $aliline = get_nth_line($alifile, $sntno);
+    my $hypline = get_nth_line($hypfile, $sntno);
+    my $haliline = get_nth_line($halifile, $sntno);
     # Print raw sentences first. No tables, to make reading easier.
     $html .= "<dl>\n";
     $html .= "  <dt><b>source</b></dt>\n";
     $html .= "  <dd>$srcline</dd>\n";
     $html .= "  <dt><b>target</b></dt>\n";
     $html .= "  <dd>$tgtline</dd>\n";
+    $html .= "  <dt><b>system hypothesis</b></dt>\n";
+    $html .= "  <dd>$hypline</dd>\n";
     $html .= "</dl>\n";
     # Decompose alignments into array of arrays (pairs).
     my @alignments = map {my @pair = split(/-/, $_); \@pair} (split(/\s+/, $aliline));
     my @srcwords = split(/\s+/, $srcline);
     my @tgtwords = split(/\s+/, $tgtline);
-    # Display the source words along with their alignment links.
-    $html .= "<table border style='font-family:Code2000'>\n";
-    $html .= "  <tr>";
-    for(my $i = 0; $i<=$#srcwords; $i++)
+    my @halignments = map {my @pair = split(/-/, $_); \@pair} (split(/\s+/, $haliline));
+    my @hypwords = split(/\s+/, $hypline);
+    # Get HTML for the three sentences with alignments.
+    my $srcrow = sentence_to_table_row(\@srcwords, \@tgtwords, \@alignments, 0);
+    my $tgtrow = sentence_to_table_row(\@tgtwords, \@srcwords, \@alignments, 1);
+    my $hyprow = sentence_to_table_row(\@hypwords, \@srcwords, \@halignments, 1);
+    # We can display all three pairs of rows in one table or we can display them in separate tables.
+    my $onetable = 0;
+    if($onetable)
     {
-        if($srcwords[$i] eq $config{word})
+        # Display the source words along with their alignment links.
+        $html .= "<table border style='font-family:Code2000'>\n";
+        # An empty row separates source and target sections.
+        $html .= join("  <tr><td></td></tr>\n", ($srcrow, $tgtrow, $hyprow));
+        $html .= "</table>\n";
+    }
+    else # separate tables
+    {
+        foreach my $rowpair ($srcrow, $tgtrow, $hyprow)
         {
-            $html .= "<td style='color:red'>$srcwords[$i]</td>";
-        }
-        else
-        {
-            # Every word except for the current one is a link to its own examples.
-            $html .= '<td>'.word_to_link($config{experiment}, 's', $srcwords[$i]).'</td>';
+            # Display the source words along with their alignment links.
+            $html .= "<table border style='font-family:Code2000'>\n";
+            $html .= $rowpair;
+            $html .= "</table>\n";
         }
     }
-    $html .= "</tr>\n";
-    # Second row contains target words aligned to source words.
-    $html .= "  <tr>";
-    for(my $i = 0; $i<=$#srcwords; $i++)
-    {
-        my $ali_word = join('&nbsp;', map {join('-', @{$_})} (grep {$_->[0]==$i} (@alignments)));
-        my $ali_ctpart = join('&nbsp;', map {$tgtwords[$_->[1]] eq $config{word} ? "<span style='color:red'>$tgtwords[$_->[1]]</span>" : $tgtwords[$_->[1]]} (grep {$_->[0]==$i} (@alignments)));
-        $html .= "<td>$ali_ctpart<br/>$ali_word</td>";
-    }
-    $html .= "</tr>\n";
-    # An empty row separates source and target sections.
-    $html .= "  <tr><td></td></tr>\n";
-    # Display the target words along with their alignment links.
-    $html .= "  <tr>";
-    for(my $i = 0; $i<=$#tgtwords; $i++)
-    {
-        my $ali_word = join('&nbsp;', map {join('-', @{$_})} (grep {$_->[1]==$i} (@alignments)));
-        my $ali_ctpart = join('&nbsp;', map {$srcwords[$_->[0]] eq $config{word} ? "<span style='color:red'>$srcwords[$_->[0]]</span>" : $srcwords[$_->[0]]} (grep {$_->[1]==$i} (@alignments)));
-        $html .= "<td>$ali_word<br/>$ali_ctpart</td>";
-    }
-    $html .= "</tr>\n";
-    $html .= "  <tr>";
-    for(my $i = 0; $i<=$#tgtwords; $i++)
-    {
-        if($tgtwords[$i] eq $config{word})
-        {
-            $html .= "<td style='color:red'>$tgtwords[$i]</td>";
-        }
-        else
-        {
-            # Every word except for the current one is a link to its own examples.
-            $html .= '<td>'.word_to_link($config{experiment}, 't', $tgtwords[$i])."</td>";
-        }
-    }
-    ###!!! If the filter is test+reference, show a third row with system hypothesis.
-    if($config{filter} eq 'r')
-    {
-        my $tgtline = get_nth_line($files->{H}, $sntno);
-        my $aliline = get_nth_line($files->{HA}, $sntno);
-        # Decompose alignments into array of arrays (pairs).
-        my @alignments = map {my @pair = split(/-/, $_); \@pair} (split(/\s+/, $aliline));
-        my @tgtwords = split(/\s+/, $tgtline);
-        $html .= "  <tr><td></td></tr>\n";
-        $html .= "  <tr>";
-        for(my $i = 0; $i<=$#tgtwords; $i++)
-        {
-            my $ali_word = join('&nbsp;', map {join('-', @{$_})} (grep {$_->[1]==$i} (@alignments)));
-            my $ali_ctpart = join('&nbsp;', map {$srcwords[$_->[0]] eq $config{word} ? "<span style='color:red'>$srcwords[$_->[0]]</span>" : $srcwords[$_->[0]]} (grep {$_->[1]==$i} (@alignments)));
-            $html .= "<td>$ali_word<br/>$ali_ctpart</td>";
-        }
-        $html .= "</tr>\n";
-        $html .= "  <tr>";
-        for(my $i = 0; $i<=$#tgtwords; $i++)
-        {
-            if($tgtwords[$i] eq $config{word})
-            {
-                $html .= "<td style='color:red'>$tgtwords[$i]</td>";
-            }
-            else
-            {
-                # Every word except for the current one is a link to its own examples.
-                $html .= '<td>'.word_to_link($config{experiment}, 't', $tgtwords[$i])."</td>";
-            }
-        }
-    }
-    $html .= "</tr>\n";
-    $html .= "</table>\n";
     return $html;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Generates a part of an HTML table with a sentence and corresponding aligned
+# words from the other language. The function generates two table rows. One row
+# contains words (tokens) of the sentence. The other row contains the
+# alignments: numeric indices and the actual tokens from the corresponding
+# sentence in the other language. The tokens in the main row are linked to
+# word example pages. Tokens in the alignment row are not hyperlinks. By
+# default, the main row is displayed first and the alignment row second. They
+# can be swapped by specifying that the sentence is in the target language.
+#------------------------------------------------------------------------------
+sub sentence_to_table_row
+{
+    # Source and target of the alignment, not necessarily source and target languages in the experiment analyzed.
+    my $srcwords = shift; # array reference
+    my $tgtwords = shift; # array reference
+    my $alignments = shift; # reference to array of arrays (pairs).
+    my $target = shift; # 0|1: influences not only the order of the table rows
+    my ($linklang, $aithis, $aithat);
+    unless($target)
+    {
+        $linklang = 's';
+        $aithis = 0;
+        $aithat = 1;
+    }
+    else
+    {
+        $linklang = 't';
+        $aithis = 1;
+        $aithat = 0;
+    }
+    my $mainrow;
+    $mainrow .= "  <tr>";
+    for(my $i = 0; $i<=$#{$srcwords}; $i++)
+    {
+        # Every word except for the current one is a link to its own examples.
+        $mainrow .= '<td>'.word_to_link($config{experiment}, $linklang, $srcwords->[$i]).'</td>';
+    }
+    $mainrow .= "</tr>\n";
+    # Second row contains target words aligned to source words.
+    my $alirow;
+    $alirow .= "  <tr>";
+    for(my $i = 0; $i<=$#{$srcwords}; $i++)
+    {
+        my $ali_word = join('&nbsp;', map {join('-', @{$_})} (grep {$_->[$aithis]==$i} (@{$alignments})));
+        my $ali_ctpart = join('&nbsp;', map {$tgtwords->[$_->[$aithat]]} (grep {$_->[$aithis]==$i} (@{$alignments})));
+        $alirow .= "<td>$ali_ctpart<br/>$ali_word</td>";
+    }
+    $alirow .= "</tr>\n";
+    # Put the two rows together.
+    my $html = $target ? $alirow.$mainrow : $mainrow.$alirow;
+    return $html;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Counts sentences (lines) in a file. It is not efficient to do this every time
+# an example is displayed, especially not for long files. We should count the
+# lines during indexing and store the number within the index.
+#------------------------------------------------------------------------------
+sub count_lines
+{
+    my $path = shift;
+    my $n;
+    open(IN, $path) or print("<p style='color:red'>Cannot read $path: $!</p>\n");
+    while(<IN>)
+    {
+        $n++;
+    }
+    close(IN);
+    return $n;
 }
 
 
