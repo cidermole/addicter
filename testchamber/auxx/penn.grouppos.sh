d15c1a5efe9863e44ae65150610085dbb3800a15
fishel <fishel@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-05-13 08:18:10 +0000

    added alignments of sources-to-hypotheses and source-to-reference done with GIZA++ together with CzEng
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2007 85e76b90-d0e1-47da-8fde-e7818187306d

--- testchamber/auxx/penn.grouppos.sh
+++ testchamber/auxx/penn.grouppos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat $@ | sed -e "s/|\([JNV]\|RB\|PDT\)[^|]*|/|Content|/g;s/|CD|/|Number|/g;s/|FW|/|Foreign|/g;s/|[^A-Z ]\+|/|Punct|/g;s/|[A-Z]\+|/|Aux|/g;s/|[WP]P\$|/|Aux|/g"
+cat $@ | sed -e "s/|\([JNV]\|RB\|PDT\)[^|]*|/|Content|/g;s/|CD|/|Number|/g;s/|FW|/|Foreign|/g;s/|\([^A-Z ]\+\|SENT\|SYM\)|/|Punct|/g;s/|[A-Z]\+|/|Aux|/g;s/|[WP]P\$|/|Aux|/g;s/|\(W.*\|TO\|UH\|DT\|EX\)|/|Aux|/g"
