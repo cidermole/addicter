4b420a6b43d15736e3226ca097e976c81ed06ad6
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-06-13 17:14:55 +0000

    adding a conversion from alitextview back to compact alignments
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2116 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ testchamber/dealitextview.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+# Converts the output of alitextview.pl back to 0-1 ...
+# Any other char than '-' in the alignment is treated as an alignment point.
+# Ondrej Bojar, bojar@ufal.mff.cuni.cz
+
+use strict;
+use warnings;
+
+binmode(STDIN, "utf8");
+binmode(STDOUT, "utf8");
+binmode(STDERR, "utf8");
+
+my @lines = ();
+my $rowlabelwidth = 0;
+my $expected_points = undef;
+my $nr = 0;
+while (<>) {
+  $nr++;
+  chomp;
+  if (/^$/) {
+    resolve(\@lines) if 0 < scalar @lines;
+    @lines = ();
+    $rowlabelwidth = 0;
+    $expected_points = undef;
+    next;
+  }
+  if (0 == $rowlabelwidth && /^( *[^ ]+ )/) {
+    # get the rowlabel width
+    $rowlabelwidth = length($1);
+  }
+  # remove the rowlabel
+  my $rowlabel = substr($_, 0, $rowlabelwidth, "");
+  # print STDERR "RL '$rowlabel' LEFT '$_'\n";
+  next if $rowlabel =~ /^\s*$/; # skip collabel lines
+  my @points = split / /, $_;
+  my $points = scalar @points;
+  $expected_points = $points if !defined $expected_points;
+  die "$nr:Bad line, expected $expected_points, got $points"
+    if $points != $expected_points;
+  push @lines, [ @points ];
+  # print STDERR "POINTS @points\n";
+}
+resolve(\@lines) if 0 < scalar @lines;
+
+sub resolve {
+  my $lines = shift;
+  my @outpoints;
+  for(my $r = 0; $r < scalar(@$lines); $r++) {
+    my @points = @{$lines->[$r]};
+    for(my $c = 0; $c < scalar(@{$lines->[$r]}); $c++) {
+      if ($lines->[$r]->[$c] ne "-") {
+        push @outpoints, $r."-".$c;
+      }
+    }
+  }
+  print "@outpoints\n";
+}
