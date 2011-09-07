8ffc3395184c40f35c00f0ca77faf3b59aea25cb
zeman <zeman@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-09-07 09:52:37 +0000

    Regular expressions can be used for word lookup.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2341 85e76b90-d0e1-47da-8fde-e7818187306d

--- cgi/index.pl
+++ cgi/index.pl
@@ -12,6 +12,7 @@ binmode(STDERR, ":utf8");
 use lib 'C:\Documents and Settings\Dan\Dokumenty\Lingvistika\lib';
 use lib '/home/zeman/lib';
 use dzcgi;
+use AddicterHTML;
 
 # Print the HTML header (so that any debugging can possibly also output to the browser).
 print("Content-type: text/html; charset=utf8\n\n");
@@ -39,17 +40,62 @@ if(exists($config{experiment}))
 {
     # Path to experiment we are analyzing (can be relative to the location of this script).
     $experiment = $config{experiment};
-    print("  <h2>Browse Test Corpus</h2>\n");
-    print("  <p><a href='browsetest.pl?experiment=$config{experiment}'>Click here.</a></p>\n");
-    print("  <h2>Explore Words in Training Corpus</h2>\n");
-    if(!exists($config{letter}))
+    print("  <h1>Current Experiment: <span style='color:red'>$experiment</span></h1>\n");
+    print("  <table border='1'>\n");
+    print("    <tr>\n");
+    print("      <td valign=top style='background:yellow'>\n");
+    print("        <h2><a href='browsetest.pl?experiment=$experiment'>Test Data Browser</a></h2>\n");
+    my $ellipsis = chr(8230); # ...
+    for(my $i = 1; $i<=5; $i++)
     {
-        print_start_letters('s');
-        print_start_letters('t');
+        my $srcline = AddicterHTML::get_nth_line("$experiment/test.src", $i);
+        my $tgtline = AddicterHTML::get_nth_line("$experiment/test.tgt", $i);
+        $srcline = substr($srcline, 0, 80).$ellipsis if(length($srcline)>80);
+        $tgtline = substr($tgtline, 0, 80).$ellipsis if(length($tgtline)>80);
+        $srcline =~ s/\s/&nbsp;/g;
+        $tgtline =~ s/\s/&nbsp;/g;
+        print("        $srcline<br/>\n");
+        print("        $tgtline<br/>\n");
     }
-    else
+    print("      </td>\n");
+    print("      <td valign=top style='background:lightblue'>\n");
+    print("        <h2>Word Explorer</h2>\n");
+    print("<form method=get action='index.pl'>\n");
+    print("  SRC:\n");
+    print("  <input type=hidden name=experiment value='$config{experiment}' />\n");
+    print("  <input type=hidden name=lang value='s' />\n");
+    my $default = $config{lang} eq 's' ? $config{re} : '';
+    print("  <input type=text name=re value='$default' />\n");
+    print_start_letters('s');
+    print("</form>\n");
+    print("<form method=get action='index.pl'>\n");
+    print("  TGT:\n");
+    print("  <input type=hidden name=experiment value='$config{experiment}' />\n");
+    print("  <input type=hidden name=lang value='t' />\n");
+    $default = $config{lang} eq 't' ? $config{re} : '';
+    print("  <input type=text name=re value='$default' />\n");
+    print_start_letters('t');
+    print("</form>\n");
+    print("      </td>\n");
+    print("    </tr>\n");
+    print("  </table>\n");
+    if(exists($config{letter}))
     {
         print_words_by_letter();
+    }
+    elsif(exists($config{re}))
+    {
+        # For safety reasons, the regular expression must not contain embedded code execution.
+        my $re = $config{re};
+        $re = '' if($re =~ m/\(\?\??\{/);
+        if($re)
+        {
+            # Escape &<> in the regular expression. It is needed for displaying the RE.
+            $re =~ s/&/&amp;/g;
+            $re =~ s/</&lt;/g;
+            $re =~ s/>/&gt;/g;
+            print_words_matching_re($config{lang}, $re);
+        }
     }
 }
 # Close the HTML document.
@@ -86,6 +132,8 @@ sub print_start_letters
 {
     # Which index to read? 's' or 't'?
     my $oprf = shift;
+    # Do we want to print the introductory sentence, too?
+    my $intro = shift;
     my $indexname = $oprf.'index.txt';
     # Read the master index (first letters of words).
     open(INDEX, "$experiment/$indexname") or print("<p style='color:red'>Cannot open $indexname!</p>\n");
@@ -94,8 +142,11 @@ sub print_start_letters
     $firstletters =~ s/\r?\n$//;
     my @firstletters = split(/\s+/, $firstletters);
     # Print list of words we can inspect.
-    my $corpus_part = $oprf eq 's' ? 'source' : $oprf eq 't' ? 'target' : "<span style='color:red'>unknown</span>";
-    print("  <p>The $corpus_part corpus contains words beginning in the following letters. Click on a letter to view the list of words beginning in that letter.</p>\n");
+    if($intro)
+    {
+        my $corpus_part = $oprf eq 's' ? 'source' : $oprf eq 't' ? 'target' : "<span style='color:red'>unknown</span>";
+        print("  <p>The $corpus_part corpus contains words beginning in the following letters. Click on a letter to view the list of words beginning in that letter.</p>\n");
+    }
     foreach my $letter (@firstletters)
     {
         print("  <a href='index.pl?experiment=$experiment&amp;lang=$oprf&amp;letter=$letter'>$letter</a>\n");
@@ -123,6 +174,63 @@ sub print_words_by_letter
     }
     close(INDEX);
     foreach my $word (sort(keys(%index)))
+    {
+        print("  <a href='example.pl?experiment=$experiment&amp;lang=$config{lang}&amp;word=$word'>$word</a>\n");
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Prints the list of words that match a regular expression.
+#------------------------------------------------------------------------------
+sub print_words_matching_re
+{
+    # Which index to read? 's' or 't'?
+    my $oprf = shift;
+    my $indexname = $oprf.'index.txt';
+    my $re = shift;
+    # Read the master index (first letters of words).
+    open(INDEX, "$experiment/$indexname") or print("<p style='color:red'>Cannot open $indexname!</p>\n");
+    my $firstletters = <INDEX>;
+    close(INDEX);
+    $firstletters =~ s/\r?\n$//;
+    my @firstletters = split(/\s+/, $firstletters);
+    # Loop over first letters, go through all index files.
+    my %index;
+    foreach my $fl (@firstletters)
+    {
+        my $indexname = sprintf("$experiment/$config{lang}index%04x.txt", ord($fl));
+        open(INDEX, $indexname) or print("<p style='color:red'>Cannot open $indexname: $!</p>\n");
+        while(<INDEX>)
+        {
+            s/\r?\n$//;
+            my ($word, $sentences) = split(/\t/, $_);
+            next unless($word =~ m/$re/);
+            my @sentences = split(/\s+/, $sentences);
+            $index{$word} = \@sentences;
+        }
+        close(INDEX);
+    }
+    # Show the words beginning in the regular expression first.
+    # Chances are that the expression actually is the exact word the user is searching for.
+    print("<p>Filter regular expression: <span style='color:green'>$re</span><br/>\n");
+    my @words = sort(keys(%index));
+    unless($re =~ m/^\^/)
+    {
+        my @words_beg = grep {$_ =~ m/^$re/} (@words);
+        my @words_rest = grep {$_ !~ m/^$re/} (@words);
+        my $nwb = scalar(@words_beg);
+        my $nwr = scalar(@words_rest);
+        print("Found $nwb words whose prefix matches the RE and $nwr other words that match the RE.</p>\n");
+        @words = (@words_beg, @words_rest);
+    }
+    else
+    {
+        my $nw = scalar(@words);
+        print("Found $nw words matching the RE.</p>\n");
+    }
+    foreach my $word (@words)
     {
         print("  <a href='example.pl?experiment=$experiment&amp;lang=$config{lang}&amp;word=$word'>$word</a>\n");
     }
