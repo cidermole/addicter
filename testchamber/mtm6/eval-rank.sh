c715a500df33c4525d6e204f74784b4c30e64022
fishel <fishel@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-09-08 12:35:25 +0000

    sdgsdg
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2360 85e76b90-d0e1-47da-8fde-e7818187306d

--- testchamber/mtm6/eval-rank.sh
+++ testchamber/mtm6/eval-rank.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #
-# given a manually annotated file and an automatically annotaged one (Hjerson's or Addicter's),
-# print a table of counts and rank correlations 
+# given a manually annotated file and an automatically annotated file, print a table row with
+# error counts and Spearman's rank correlations between them
 #
-# input: manual_file automatic_file; both in Hjerson's output format (with flags reversed):
+# input: manual_file automatic_file
+#
+# required format for both files:
 # 
 # 1::ref-err-cats: the reord~~total reord~~amount miss~~designated for assistance to reord~~the reord~~system is to be divided into two parts .  
 # 1::hyp-err-cats: the for reord~~the reord~~system to help ext~~certain reord~~total reord~~amount will be divided into two parts .  
@@ -13,3 +15,54 @@
 # 2::hyp-err-cats: lex~~here too there infl~~is price and quality categories .  
 # ...
 #
+
+manfile=$1
+autofile=$2
+
+if [[ -z "$manfile" || -z "$autofile" ]]
+then
+	echo "Usage: eval-rank.sh manual_file automatic_file" 1>&2
+	exit 1
+fi
+
+#print header
+output="|| $autofile ||"
+table="evals man auto\n"
+
+#print a row of numbers
+for errtype in infl reord miss ext lex
+do
+	table="$table$errtype "
+	
+	if [[ $errtype == "ext" ]]
+	then
+		togrep=hyp
+	else
+		togrep=ref
+	fi
+	
+	first=yes
+	
+	for analysisfile in $manfile $autofile
+	do
+		val=$( cat $analysisfile | grep "$togrep-err-cats" | grep -o "[^ ]\+" | grep -c "$errtype~~" )
+		
+		if [[ -z $first ]]
+		then
+			output="$output/$val ||"
+		else
+			first=""
+			output="$output $val"
+		fi
+		
+		table="$table$val "
+	done
+	
+	table="$table\n"
+done
+
+rho=$( printf "$table" | ./auxx/spearmans-rank.perl | xargs echo | cut -d " " -f 5 )
+
+output="$output $rho ||\n"
+
+printf "$output";
