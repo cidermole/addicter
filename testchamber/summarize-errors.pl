eda94e1aedec37d839eaa87486ee702f80f51627
fishel <fishel@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-09-08 16:13:41 +0000

    added an error flag summarization script + minor
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2372 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ testchamber/summarize-errors.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+
+#
+# produces a summary table of error frequencies from Hjerson't output format:
+#
+# 1::ref-err-cats: the reord~~total reord~~amount miss~~designated for assistance to reord~~the reord~~system is to be divided into two parts .  
+# 1::hyp-err-cats: the for reord~~the reord~~system to help ext~~certain reord~~total reord~~amount will be divided into two parts .  
+# 
+# 2::ref-err-cats: lex~~there infl~~are price lex~~and qualitative categories here reord~~as reord~~well .  
+# 2::hyp-err-cats: lex~~here too there infl~~is price and quality categories .  
+# ...
+#
+
+my $stats;
+my $sizes;
+
+while (<STDIN>) {
+	s/\n//g;
+	
+	my @tokens = split(/ /);
+	
+	my $rawClass = shift @tokens;
+	
+	my ($idx, $class) = split(/::/, $rawClass, 2);
+	
+	for my $token (@tokens) {
+		my ($flag, $word) = ($token =~ /~~/)? split(/~~/, $token, 2): ('none', $token);
+		
+		$sizes->{$class}++;
+		
+		$stats->{$class}->{$flag}++;
+	}
+}
+
+printf("%-12s %-12s %-6s %-s\n\n", "file", "error-flag", "count", "frequency");
+
+for my $class (sort keys %$stats) {
+	for my $flag (sort keys %{$stats->{$class}}) {
+		printf("%-12s %-12s %-6d %-.3f%%\n", $class, $flag,
+			$stats->{$class}->{$flag},
+			$stats->{$class}->{$flag} / $sizes->{$class});
+	}
+	print "\n";
+}
