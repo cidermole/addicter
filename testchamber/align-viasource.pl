834796f11ab6874a375f7bdc7efaedda10c81e6f
berka.jan <berka.jan@85e76b90-d0e1-47da-8fde-e7818187306d>
2012-02-06 16:25:38 +0000

    refhyp alignment via source to ref and source to hyp alignments
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk/addicter@2790 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ testchamber/align-viasource.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+# alignment via source
+# Copyright © 2012 Jan Berka <berka@ufal.mff.cuni.cz>
+# License: GNU GPL
+
+use io;
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+use Getopt::Long;
+use File::Spec;
+use parse;
+
+BEGIN {
+	#include packages from same folder where the
+	#script is, even if launched from elsewhere
+	
+	my @dirs = File::Spec->splitdir(File::Spec->rel2abs(File::Spec->canonpath($0)));
+	pop @dirs;
+	push(@INC, File::Spec->catdir(@dirs));
+}
+
+my ($srcRefFile, $srcHypFile) = processInputArgsAndOpts();
+my ($fhSrcRef, $fhSrcHyp) = io::openMany($srcRefFile, $srcHypFile);
+
+
+while(my $tuple = io::readSentences($fhSrcRef, $fhSrcHyp)) {
+	print("$tuple->[0]\n");
+	print("$tuple->[1]\n");
+	my $alignment = ViaSourceAlignment($tuple->[0], $tuple->[1]);
+	print("@$alignment \n");
+}
+
+############################
+#        SUBROUTINES       #
+############################
+
+
+sub processInputArgsAndOpts {
+	my ($srcRefFile, $srcHypFile) = @ARGV;
+	if (!$srcRefFile or !$srcHypFile) {
+		die("Usage: align-viasource.pl src-ref_alignment_file src-hyp_alignment_file\nRequired arguments: source to reference alignment file, source to hypothesis alignment file");
+	}
+	return ($srcRefFile, $srcHypFile);
+}
+
+sub ViaSourceAlignment {
+	my $srcRef = shift;
+	my $srcHyp = shift;
+	
+	my @viasource = qw();
+	
+	my @srcRef = split(/ /,$srcRef);
+	my @srcHyp = split(/ /,$srcHyp);
+	
+	my @srsrc = qw();
+	my @srref = qw();
+	my @shsrc = qw();
+	my @shhyp = qw();
+	
+	foreach my $token (@srcRef) {
+		my @p = split(/-/, $token);
+		push(@srsrc, $p[0]);
+		push(@srref, $p[1]);
+	}
+	foreach my $token (@srcHyp) {
+		my @p = split(/-/, $token);
+		push(@shsrc, $p[0]);
+		push(@shhyp, $p[1]);
+	}
+	
+	for my $i (0..$#srsrc) {
+		for my $j (0..$#shsrc) {
+			if ($srsrc[$i] == $shsrc[$j]) {
+				push(@viasource, $srref[$i]."-".$shhyp[$j]); 
+			}
+		}
+	}
+	return \@viasource;
+}
